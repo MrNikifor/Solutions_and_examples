9e9a227d6cfc649828c31188ada9cb8dc9c867c8
osian <oisa.nikifor@gmail.com>
2023-04-23 23:22:19 +0300

    Examples

--- src/string/PalindromeRevers.java
+++ src/string/PalindromeRevers.java
@@ -2,11 +2,14 @@ package string;
 // Определить слово полиндром или нет.
 public class PalindromeRevers {
     public static void main(String[] args) {
-        // Решение
+        // Решение 1
         String str = "А роза упала на лапу азора";
         if(palindrome(str)) System.out.println(str + " - ваше слово полиндром");
         else System.out.println(str + " - ваше слово не полиндром");
+
     }
+    // Решение 1
+    /*
     public static boolean palindrome(String x){
         // убираем пробелы, преобразуем в нижний регистр
         String notSpase = x.replace(" ", "").toLowerCase();
@@ -17,5 +20,14 @@ public class PalindromeRevers {
             if (notSpChar[i] != notSpChar[notSpChar.length - i -1]) return false;
         }
         return true;
+    }*/
+    public static boolean palindrome(String str1){
+        // преобразуем в нижний регистр, убираем пробелы
+        str1 = str1.toLowerCase().replace(" ", "");
+        // строку приобразуем в StringBuilder, переворачивам и преобразуем обратно в строку
+        String revers_str = new StringBuilder(str1).reverse().toString();
+        // сравниваем  строки с помощи equals на символы
+        return str1.equals(revers_str);
     }
+
 }
